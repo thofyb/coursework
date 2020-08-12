ceb0c1210bf06c6f1e9f81a57c08c95c545ecd95
thofyb <bogdyich@gmail.com>
2020-08-12 19:54:23 +0000

    anonymous class implemented

--- inlining/app/src/main/java/com/example/inlining/MainActivity.java
+++ inlining/app/src/main/java/com/example/inlining/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.inlining;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,7 +10,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView mResultTextView;
     Button mStartTestButton;
-    int MAX_CYCLES = 1000000;
+    static int MAX_CYCLES = 1000000;
+    int TEST_NUMBER = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,101 +19,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mResultTextView = (TextView) findViewById(R.id.tv_result);
+        mResultTextView.append("(anonymous class, opt on)\n");
 
         mStartTestButton = (Button)  findViewById(R.id.b_startTest);
+        mStartTestButton.setText("Start " + TEST_NUMBER + " tests");
         mStartTestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Thread calc = new Thread(new CalcTask());
-                long startTime, endTime;
-                startTime = System.currentTimeMillis();
-                calc.start();
-                try {
-                    calc.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                long avg = 0;
+                for (int i = 0; i < TEST_NUMBER; i++) {
+                    Thread calc = new Thread(new Runnable() {
+                        public int cycles = MAX_CYCLES;
+
+                        @Override
+                        public void run() {
+                            for (int i = 0; i < cycles; i++) {
+                                calcFunc();
+                                calcFunc();
+                                calcFunc();
+                                calcFunc();
+                                calcFunc();
+                                calcFunc();
+                                calcFunc();
+                                calcFunc();
+                                calcFunc();
+                                calcFunc();
+                            }
+                        }
+
+                        private void calcFunc() { System.currentTimeMillis(); }
+                    });
+
+                    long startTime, totalTime;
+                    startTime = System.currentTimeMillis();
+                    calc.start();
+                    try {
+                        calc.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    totalTime = System.currentTimeMillis();
+                    totalTime -= startTime;
+                    mResultTextView.append(i + ".  Execution time: " + totalTime + "ms\n");
+                    avg += totalTime;
                 }
-                endTime = System.currentTimeMillis();
-                mResultTextView.append("Execution time: " + (endTime - startTime) + "ms\n");
+                mResultTextView.append("Average time: " + (avg / TEST_NUMBER));
             }
         });
-
-//        new CalcTask().execute(100000);
-
-//        mResultTextView.setText("");
-//        mResultTextView.append("Execution time: " + totalTime / 1000 + " s");
     }
-
-    private class CalcTask implements Runnable {
-        @Override
-        public void run() {
-            for (int i = 0; i < MAX_CYCLES; i++) {
-                calcFunc();
-                calcFunc();
-                calcFunc();
-                calcFunc();
-                calcFunc();
-                calcFunc();
-                calcFunc();
-                calcFunc();
-                calcFunc();
-                calcFunc();
-//                            Log.i("com.example.inlining", String.valueOf(i));
-//                            publishProgress(i);
-            }
-        }
-
-        private void calcFunc() {
-            System.currentTimeMillis();
-        }
-    }
-
-//    private void calcFunc() {
-//        System.currentTimeMillis();
-//    }
-
-//    private class CalcTask extends AsyncTask<Integer, Integer, Long> {
-//        int MAX_CYCLES = 1000;
-//
-//        @Override
-//        protected Long doInBackground(Integer... params) {
-//            MAX_CYCLES = params[0];
-//            long startTime = System.currentTimeMillis();
-//
-//            for (int i = 0; i < MAX_CYCLES; i++) {
-//                calcFunc();
-//                calcFunc();
-//                calcFunc();
-//                calcFunc();
-//                calcFunc();
-//                calcFunc();
-//                calcFunc();
-//                calcFunc();
-//                calcFunc();
-//                calcFunc();
-//                //Log.i("com.example.inlining", String.valueOf(i));
-//                publishProgress(i);
-//            }
-//
-//            long endTime = System.currentTimeMillis();
-//
-//            return endTime - startTime;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Long aLong) {
-//            mResultTextView.setText("Execution time: " + aLong + "ms");
-//        }
-//
-//        @Override
-//        protected void onProgressUpdate(Integer... values) {
-//            mResultTextView.setText("Curr counter: " + values[0] + " of " + MAX_CYCLES);
-//        }
-//
-//        private void calcFunc() {
-//            System.currentTimeMillis();
-//        }
-//    }
-
-
 }
