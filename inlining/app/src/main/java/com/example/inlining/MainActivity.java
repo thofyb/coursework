6ba34a84c41dd354df15e75e5ab7060c192c576b
thofyb <bogdyich@gmail.com>
2020-09-07 00:32:26 +0000

    migration done

--- inlining/app/src/main/java/com/example/inlining/MainActivity.java
+++ inlining/app/src/main/java/com/example/inlining/MainActivity.java
@@ -4,33 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.os.Process;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     TextView mResultTextView;
     Button mStartTestButton;
-    Button check;
     static String TAG = "INLINING";
 
     static int MAX_CYCLES = 1000000;
     int TEST_NUMBER = 15;
-    public volatile static long TOTAL_TIME = 0;
     public volatile static long TOTAL_ETIME = 0;
-    public static int MASK = 0b1000;
-    public static int CORE = 1;
+    public static int CORE = 0;
+    public static int MASK = (int) Math.pow(2, CORE);
     public static int CURR_ATTEMPT = 0;
     static int PID = 0;
+
+    public int MIN_FREQ = 400000;
+    public int MAX_FREQ = 1500000;
 
 
     static {
@@ -46,23 +45,33 @@ public class MainActivity extends AppCompatActivity {
 
         mResultTextView = (TextView) findViewById(R.id.tv_result);
         mResultTextView.setText("PID: " + Process.myPid() + "\n");
-        mResultTextView.append("(nested class, opt on, mask = " + MASK + ")\n");
+        mResultTextView.append("(nested class, opt off, mask = " + MASK + ")\n");
 
         mStartTestButton = (Button)  findViewById(R.id.b_startTest);
         mStartTestButton.setText("Start " + TEST_NUMBER + " tests");
         mStartTestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                long avg = 0;
                 long avge = 0;
-                Thread warmup = new Thread((new CalcTask()));
-                warmup.start();
-                try {
-                    warmup.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+                //Getting min and max freq for future changing
+
+                //MIN_FREQ = Integer.parseInt(execCMD("cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"));
+                //MAX_FREQ = Integer.parseInt(execCMD("cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"));
+
+                executeCMDWrite("echo " + MAX_FREQ + " > /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq");
+
+                Thread warmup;
+                for (int i = 0; i < 3; i++) {
+                    warmup = new Thread((new CalcTask()));
+                    warmup.start();
+                    try {
+                        warmup.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-                mResultTextView.append("W.  Execution time: " + TOTAL_TIME + "ms, " + TOTAL_ETIME + "\n");
+                mResultTextView.append("W.  Execution time: " + TOTAL_ETIME + "ms\n");
 
                 for (int i = 1; i <= TEST_NUMBER; i++) {
                     CURR_ATTEMPT = i;
@@ -73,40 +82,23 @@ public class MainActivity extends AppCompatActivity {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    mResultTextView.append(i + ".  Execution time: " + TOTAL_TIME + "ms, " + TOTAL_ETIME + "\n");
-                    avg += TOTAL_TIME;
+                    mResultTextView.append(i + ".  Execution time: " + TOTAL_ETIME + "ms\n");
                     avge += TOTAL_ETIME;
                 }
-                mResultTextView.append("Average time: " + (avg / TEST_NUMBER) + ", " + (avge / TEST_NUMBER));
-            }
-        });
+                mResultTextView.append("Average time: " + (avge / TEST_NUMBER));
 
-        check = findViewById(R.id.check);
-        check.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mResultTextView.setText("");
-                mResultTextView.append("PID: " + PID + "\n");
-                String str = "cat /proc/" + PID + "/stat";
-                mResultTextView.append(str + "\n");
-                mResultTextView.append(execCMD(str));
-
+                executeCMDWrite("echo " + MIN_FREQ + " > /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq");
             }
         });
     }
 
     private static class CalcTask implements Runnable {
         public int cycles = MAX_CYCLES;
-        String cmd = "cat /sys/devices/system/cpu/cpu" + CORE + "/cpufreq/stats/time_in_state";
-        static Runtime runtime = Runtime.getRuntime();
 
         @Override
         public void run() {
             setAffinity(MASK);
-            int pid = PID;
-            long startTime, endTime, startETime, endETime, startPTime, endPTime;
-            int[] startProc, endProc;
-            List<Pair<Integer, Integer>> stats1, stats2;
+            long startETime, endETime;
 
             Log.d(TAG, "run: ====================================================================");
             Log.d(TAG, "run: CURRENT TEST: " + CURR_ATTEMPT);
@@ -157,6 +149,20 @@ public class MainActivity extends AppCompatActivity {
             return output.toString();
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void executeCMDWrite(String cmd){
+        try {
+            java.lang.Process p = Runtime.getRuntime().exec("su");
+            DataOutputStream dos = new DataOutputStream(p.getOutputStream());
+            DataInputStream is = new DataInputStream(p.getInputStream());
+            dos.writeBytes(cmd + "\nexit\n");
+            dos.flush();
+            dos.close();
+            p.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
