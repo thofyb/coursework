fe86580c194132acbb0e1a4b7aa4c4194a2a17ac
Yegor <bogdyich@gmail.com>
2020-11-18 10:31:48 +0300

    tests and measurements implemented for enum of 3 elements

--- eoan/app/src/main/cpp/affinity-lib.cpp
+++ eoan/app/src/main/cpp/affinity-lib.cpp
@@ -11,8 +11,8 @@
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_example_inlining_MainActivity_00024CalcTask_testJNI(JNIEnv *env, jobject thiz) {
-    __android_log_print(ANDROID_LOG_DEBUG, "INLINING_JNI", "lmao, this works");
+Java_com_example_eoan_MainActivity_00024CalcTask_testJNI(JNIEnv *env, jobject thiz) {
+    __android_log_print(ANDROID_LOG_DEBUG, "EOAN_JNI", "lmao, this works");
 }
 
 void setCurrentThreadAffinityMask(int mask) {
@@ -28,34 +28,33 @@ void setCurrentThreadAffinityMask(int mask) {
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_example_inlining_MainActivity_00024CalcTask_checkAffinity(JNIEnv *env, jobject thiz) {
+Java_com_example_eoan_MainActivity_00024CalcTask_checkAffinity(JNIEnv *env, jobject thiz) {
     unsigned long mask = 0;
     int err, syscallres;
     pid_t pid = gettid();
     syscallres = syscall(__NR_sched_getaffinity, pid, sizeof(mask), &mask);
     if (syscallres) {
         err = errno;
-        __android_log_print(ANDROID_LOG_ERROR, "INLINING_JNI", "Error in the syscall getaffinity: mask=%d=0x%x err=%d=0x%x", mask, mask, err, err);
+        __android_log_print(ANDROID_LOG_ERROR, "EOAN_JNI", "Error in the syscall getaffinity: mask=%d=0x%x err=%d=0x%x", mask, mask, err, err);
 //        LOGE("Error in the syscall setaffinity: mask=%d=0x%x err=%d=0x%x", mask, mask, err, err);
     } else {
-        __android_log_print(ANDROID_LOG_DEBUG, "INLINING_JNI", "%d=0x%x", mask, mask);
+        __android_log_print(ANDROID_LOG_DEBUG, "EOAN_JNI", "%d=0x%x", mask, mask);
     }
 }
 
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_example_inlining_MainActivity_00024CalcTask_setAffinity(JNIEnv *env, jobject thiz, jint arg) {
+Java_com_example_eoan_MainActivity_00024CalcTask_setAffinity(JNIEnv *env, jobject thiz, jint arg) {
     unsigned int mask = (unsigned int) arg;
     int err, syscallres;
     pid_t pid = gettid();
     syscallres = syscall(__NR_sched_setaffinity, pid, sizeof(mask), &mask);
     if (syscallres) {
         err = errno;
-        __android_log_print(ANDROID_LOG_ERROR, "INLINING_JNI", "Error in the syscall setaffinity: mask=%d=0x%x err=%d=0x%x", mask, mask, err, err);
+        __android_log_print(ANDROID_LOG_ERROR, "EOAN_JNI", "Error in the syscall setaffinity: mask=%d=0x%x err=%d=0x%x", mask, mask, err, err);
 //        LOGE("Error in the syscall setaffinity: mask=%d=0x%x err=%d=0x%x", mask, mask, err, err);
     } else {
-        __android_log_print(ANDROID_LOG_DEBUG, "INLINING_JNI", "set mask %d=0x%x", mask, mask);
+        __android_log_print(ANDROID_LOG_DEBUG, "EOAN_JNI", "set mask %d=0x%x", mask, mask);
     }
 }
-
